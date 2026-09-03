626352f84b5bd9d372822fef8861df923cac1e70
noheart-13 <2551752913@qq.com>
2026-09-03 16:35:28 +0800

    day4
    交互的学习和碰撞判断

--- Kitchen/Assets/_Assets/Scripts/Player.cs
+++ Kitchen/Assets/_Assets/Scripts/Player.cs
@@ -9,17 +9,71 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleMovement();
+        HandleOnInteractions();
+    }
+    private void HandleOnInteractions()
+    {
+        float interactDistance = 2f;
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir*speed*Time.deltaTime;
-
-        isWalking = moveDir != Vector3.zero;
-
-        float rotateSpeed = 10f;
-        transform.forward =  Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        if(Physics.Raycast(transform.position, moveDir, out RaycastHit raycastHit, interactDistance))
+        {
+           Debug.Log("Raycast hit: " + raycastHit.collider.gameObject.name);
+        }else
+        {
+            Debug.Log("Raycast did not hit anything.");
+        }
     }
-   public bool IsWalking()
+    public bool IsWalking()
     {
         return isWalking;
     }
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        float moveDistance = speed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);// Check if the player can move in the desired direction without colliding with obstacles
+        /*
+         * transform.position胶囊底部球体的中心点，也就是 point1
+         * transform.position + Vector3.up * playerHeight胶囊顶部球体的中心点，也就是 point2
+         * playerHeight当前位置向上偏移
+         * playerRadius胶囊体的半径
+         * moveDir胶囊体扫描的方向
+         * moveDistance最多向前检测多远
+         */
+        if (!canMove)
+        {
+            // If the player cannot move in the desired direction, try moving only along the X axis
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // If the player cannot move along the X axis, try moving only along the Z axis
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
 }
