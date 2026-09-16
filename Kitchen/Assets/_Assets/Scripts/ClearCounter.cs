ba6fcb7161681d22f74840b9f3c8fcff0369e576
noheart-13 <2551752913@qq.com>
2026-09-16 23:13:15 +0800

    day 8
    生成食物（西红柿，奶酪）加可视化编辑

--- Kitchen/Assets/_Assets/Scripts/ClearCounter.cs
+++ Kitchen/Assets/_Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interacting with ClearCounter");
+        Transform kitchenObjectTranform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTranform.localPosition = Vector3.zero;
     }
 }
